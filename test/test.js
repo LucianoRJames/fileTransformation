3d9185c3959a92225c50f0f082fefcf31dbe8dff
Luciano James <Luciano.James-CIC@ibm.com>
2021-12-13 14:14:39 +0000

    added read files tests

--- test/test.js
+++ test/test.js
@@ -41,3 +41,16 @@ describe("getFileNames", function () {
     ).to.throw("The directory is empty");
   });
 });
+describe("readFile", function () {
+  const readFile = fileTransformation.__get__("readFile");
+  it("Given the function receives a file name as a string it should return a string", function () {
+    assert.equal(typeof readFile("assert1.yaml"), "string");
+  });
+
+  it("Given the function receives a file name as a string it should return its data as a string", function () {
+    assert.equal(
+      readFile("assert1.yaml"),
+      'description: "this is my product" team: integration productName: address finder filters: #filter options asset_type: "REST API" deprecated: true visibility: "Internal"'
+    );
+  });
+});
